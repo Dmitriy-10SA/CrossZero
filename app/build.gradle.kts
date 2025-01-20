0b3769b870993cc8e0e75c186869916b85a8c747
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-01-20 12:57:05 +0400

    Creating main screen. Add svg res

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.andef.crosszero"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.andef.crosszero"
--- app/src/main/java/com/andef/crosszero/MainActivity.kt
+++ app/src/main/java/com/andef/crosszero/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.andef.crosszero
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonStartGame: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        buttonStartGame = findViewById<Button?>(R.id.buttonStartGame).apply {
+            setOnClickListener {
+                goToGameScreen()
+                finish()
+            }
+        }
+    }
+
+    private fun goToGameScreen() {
     }
 }
